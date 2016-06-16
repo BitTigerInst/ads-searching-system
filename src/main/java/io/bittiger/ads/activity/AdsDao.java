d42abe31ed78a0d8341ffa9ac30ffa782be974e8
Tao Xu <dojiangv@users.noreply.github.com>
2016-06-16 16:10:19 -0700

    add inverted index
    1) add method getAds(String key) for inverted index
    
    2) modify set new Ad:
       This is temp since spymemcached does not support traverse every keys, which is needed for initial inverted index
       2.1) add Ad to forward index
       2.2) if find invKey then add ad to get(invKey)
       2.3) if not invKey then set(invKey, time, ad)
    
    3) In the next version: 
       3.1) add Ad to forward index
       2.2) if find invKey in LRU then add ad to get(invKey)
       2.3) if not invKey then do noting;
       2.4) the key value pair <String invKey, Set<Ad>> will be created in AdsSelect.getInstance().traverseFwdIndex() when the keyword (not in LRU) is queried at the first time

--- src/main/java/io/bittiger/ads/activity/AdsDao.java
+++ src/main/java/io/bittiger/ads/activity/AdsDao.java
@@ -3,6 +3,9 @@ package io.bittiger.ads.activity;
 import io.bittiger.ads.model.Ad;
 import net.spy.memcached.MemcachedClient;
 
+import java.util.Set;
+import java.util.HashSet;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
@@ -39,6 +42,18 @@ public class AdsDao {
         System.out.println(object);
     }
 
+
+    /****** Inverted Index ******/
+    public Set<Ad> getAds(String key) {
+        try {
+            return (Set<Ad>) getCache().get(key);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /****** Forward Index ******/
     public Ad getAd(long key) {
         try {
             return (Ad)getCache().get(Long.toString(key));
@@ -50,12 +65,35 @@ public class AdsDao {
 
     public boolean setAd(Ad ad) {
         String key = Long.toString(ad.getAdId());
+        String fwdKey = "fwd" + key;
+
         try {
-            getCache().set(key, 3600, ad);
+            MemcachedClient cache = getCache();
+            
+            /****** Add one single ad to fwd index ******/
+            cache.set(fwdKey, 3600, ad);
+
+            /****** Add one ad to inv index if invKey exist ******/
+            String[] keywords = ad.getKeywords();
+            for (String keyword : keywords) {
+                String invKey = "inv" + keyword;
+                Set<Ad> ads = (Set<Ad>) cache.get(invKey);
+                if (ads != null) {
+                    ads.add(ad);
+                } else {
+                    /****** Add one ad to inv index when invKey does not exist. 
+                     * This will be moved to AdsSelect.getInstance().traverseFwdIndex() 
+                     * after we find a way to traverse the fwdKey in spymemcached******/
+
+                    ads = new HashSet<Ad>();
+                    ads.add(ad);
+                    cache.set(invKey, 3600, ads);
+                }
+            }
+            return true;
         } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
-        return true;
     }
 }
