cbf3960a9f9cc5a569c1b9ee73623b357e59724e
Tao Xu <tao.xu82@gmail.com>
2016-06-23 00:57:37 -0700

    Add test case for getAds(String invKey)

--- src/test/java/io/bittiger/ads/activity/AdsDaoTest.java
+++ src/test/java/io/bittiger/ads/activity/AdsDaoTest.java
@@ -3,6 +3,7 @@ package io.bittiger.ads.activity;
 import io.bittiger.ads.model.Ad;
 import org.junit.Test;
 import java.lang.String;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
@@ -25,10 +26,19 @@ public class AdsDaoTest {
         AdsDao.getInstance().setAd(ad);
 
         Ad res = AdsDao.getInstance().getAd(expectedAdId);
+        Set<Ad> adSet = AdsDao.getInstance().getAds("invkey1");
         String[] currKeywords = res.getKeywords();
+        boolean hasAd = false;
+        for (Ad tmp : adSet) {
+            if (tmp.getAdId() == expectedAdId) {
+                hasAd = true;
+                break;
+            }
+        }
 
         assertEquals(keywords[0], currKeywords[0]);
         assertEquals(expectedBid, res.getBid(), 0.001);
         assertEquals(expectedCampaignId, res.getCampaignId(), 0.001);
+        assertEquals(hasAd, true);
     }
 }
